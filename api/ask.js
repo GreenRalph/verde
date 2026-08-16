fe95b90f5a44c9653d08086467f3592ca936d91f
GreenRalph <tentaw13@gmail.com>
2026-08-16 16:43:30 +0800

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const KEY = process.env.GEMINI_API_KEY;
+  const KEY = process.env.GEMINI_API_KEY || process.env.GEMINI_KEY;
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
